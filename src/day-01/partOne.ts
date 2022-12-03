ebe580e678bc80dd4f49972193262f0508a70e2c
Nick Cunningham <nick.cunningham@leadcrunch.ai>
2022-12-02 18:01:27 -0800

    cleanup

--- src/day-01/partOne.ts
+++ src/day-01/partOne.ts
@@ -1,1 +1,30 @@
-export default function partOne(input) {}
+type ElfCounts = {
+  [elfid: number]: number;
+  highestIndex: number;
+  highestValue: number;
+};
+
+export default function partOne(input) {
+  const elfInventories = input.split('\n\n');
+  const elfQuantities = elfInventories.reduce(
+    (accumulator: ElfCounts, currentValue: string, i: number) => {
+      const amounts = currentValue.split('\n');
+      const totalCalories = amounts.reduce(
+        (total, calories) => Number(total) + Number(calories),
+        0
+      );
+
+      accumulator[i] = totalCalories;
+
+      if (totalCalories > accumulator[accumulator.highestIndex]) {
+        accumulator.highestIndex = i;
+        accumulator.highestValue = totalCalories;
+      }
+
+      return accumulator;
+    },
+    {highestIndex: 0, highestValue: 0}
+  );
+
+  console.log(elfQuantities.highestValue);
+}
--- src/day-01/partTwo.ts
+++ src/day-01/partTwo.ts
@@ -1,1 +1,15 @@
-export default function partTwo(input) {}
+export default function partTwo(input) {
+  const elfInventories = input.split('\n\n');
+  const elfQuantities = elfInventories.map((inventory: string, i: number) => {
+    const amounts = inventory.split('\n');
+    const totalCalories = amounts.reduce(
+      (total, calories) => Number(total) + Number(calories),
+      0
+    );
+    return totalCalories;
+  });
+
+  elfQuantities.sort((a: number, b: number) => b - a);
+
+  console.log(elfQuantities[0] + elfQuantities[1] + elfQuantities[2]);
+}
